2e06cb9dcce0f19f68fb436d016d697f88e95506
Jaeyeol Song <songjy92@gmail.com>
2024-09-06 00:28:46 +0900

    rename the homepage file

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
-
+st.title("Song's FullstackGPT Streamlit Page")
 st.markdown(
     """
 #Hello!
 
 Welcome to my FullstackGPT Portfolio!
 Here are the apps I made:
+This Streamlit page is implemented based on the nomadcoder's lectures. 
 
 - [DocumentGPT](/DocumentGPT)
 - [PrivateGPT](/PrivateGPT)
